67721a83e9d4d4c8e8c3b0ea35869b062d47f8fc
pirogoeth <pirogoeth@maio.me>
2014-08-13 11:55:40 -0500

    0.1: removed debugging keybind

--- tedit.c
+++ tedit.c
@@ -11,11 +11,6 @@
 #include "screen.h"
 #include "statusline.h"
 
-void kill_window(Screen *screen, int i) {
-    screen_deinit(screen);
-    screen_close(screen);
-}
-
 int main(int argc, char *argv[]) {
     char *filepath;
     filepath = argv[1];
@@ -35,7 +30,6 @@ int main(int argc, char *argv[]) {
     screen_set_active_fmanager(screen, tfm);
 
     Keybinder *kb = keybinder_create(screen);
-    keybinding_create(kb, 'x', kill_window);
 
     Statusline *sl = statusline_create(screen);
 
